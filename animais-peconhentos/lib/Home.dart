5ef71e363a52730e6b79ab81a3e1d0a08d771cad
BRUNO H. TECHEIRA <bruno.bht.bh@gmail.com>
2020-06-01 22:18:51 -0300

    fonte vermelhar em chamada de emergencia

--- animais-peconhentos/lib/Home.dart
+++ animais-peconhentos/lib/Home.dart
@@ -84,7 +84,7 @@ class _HomeState extends State<Home> {
               child: Text(
                 "Chamada de emergência",
                 style: TextStyle(
-                  color: Colors.white
+                  color: Colors.red
                 ),
               )
             )
