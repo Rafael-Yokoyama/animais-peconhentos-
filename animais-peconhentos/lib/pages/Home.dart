e2524713c585b1c53df7269b0ba6e96ae8c5a187
BRUNO H. TECHEIRA <bruno.bht.bh@gmail.com>
2020-06-13 16:58:39 -0300

    tela de contato de emergência
    Adicionada a tela para atualizar o contato de emergência

--- animais-peconhentos/lib/pages/Home.dart
+++ animais-peconhentos/lib/pages/Home.dart
@@ -296,7 +296,7 @@ class _HomeState extends State<Home> {
         context,
         MaterialPageRoute(
           builder: (context){
-            return ContatoDeEmergencia();
+            return ContatoDeEmergencia(this._idUsuario);
           }
         )
       );
