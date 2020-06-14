51fcc184588a9fa14e2395794a410f82114376ef
Rafael <rafaelyokoyama925@gmail.com>
2020-06-14 12:54:16 -0300

    debug off
    tirado o debug das telas

--- animais-peconhentos/lib/pages/Prevencoes.dart
+++ animais-peconhentos/lib/pages/Prevencoes.dart
@@ -10,6 +10,7 @@ class Prevencoes extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+
         backgroundColor: Colors.black,
         title: Center(
           child: Text(
